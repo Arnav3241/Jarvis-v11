dfbe8966b2571445fc8523f06a1855c430242aa5
Avi Sinha <iamavisinha@gmail.com>
2024-03-04 18:53:38 +0530

    Working speak function (test 2) [PI.AI text to speech]

--- tests/2_Speak.py
+++ tests/2_Speak.py
@@ -9,33 +9,49 @@ import os
 # env = json.loads(f"{os.getcwd()}/env.local.json")
 # env = json.loads(f"{os.getcwd()}\\env.local.json")
 
-with open(f"{os.getcwd()}\\env.local.json", 'r') as j:
-  env = json.loads(j.read())
+def setup():
+  with open(f"{os.getcwd()}\\env.local.json", 'r') as j:
+    env = json.loads(j.read())
 
-print(env)
-service = Service(ChromeDriverManager().install())
+  service = Service(ChromeDriverManager().install())
+  driver = webdriver.Chrome(service=service)
+  driver.set_window_position(-10000, 0)
+  website = "https://pi.ai/talk"
 
-driver = webdriver.Chrome(service=service)
-website = "https://pi.ai/talk"
-text = "To calculate the probability of winning more than one giveaway, we can use the binomial probability formula."
+  driver.get(website)
+  time.sleep(3)
+  driver.find_element(by=By.XPATH, value="//*[@id=\"__next\"]/main/div/div/button").click()
+  time.sleep(3)
+  driver.find_element(by=By.XPATH, value="//*[@id=\"__next\"]/main/div/div/button").click()
+  time.sleep(3)
+  driver.find_element(by=By.XPATH, value="//*[@id=\"__next\"]/main/div/div/div[1]/div[1]/div[2]/button[2]").click()
+  time.sleep(3)
+  driver.find_element(by=By.XPATH, value="//*[@id=\"__next\"]/main/div/div/div/div/div/div[1]/div/div[1]/button[2]").click()
+  time.sleep(3)
+  driver.find_element(by=By.XPATH, value="//*[@id=\"email\"]").send_keys(env["Facebook_Em"])
+  time.sleep(3)
+  driver.find_element(by=By.XPATH, value="//*[@id=\"pass\"]").send_keys(env["Facebook_Pw"])
+  time.sleep(3)
+  driver.find_element(by=By.XPATH, value="//*[@id=\"loginbutton\"]").click()
+  time.sleep(10)
+  driver.find_element(by=By.XPATH, value="//*[@id=\"__next\"]/main/div/div/div[3]/div[1]/div[4]/div/div/textarea").send_keys("Reply with \"....\"")
+  time.sleep(3)
+  driver.find_element(by=By.XPATH, value="//*[@id=\"__next\"]/main/div/div/div[3]/div[1]/div[4]/div/button").click()
+  time.sleep(3)
+  driver.find_element(by=By.XPATH, value="//*[@id=\"__next\"]/main/div/div/div[3]/div[2]/div[2]/div/div[2]/button").click()
 
-driver.get(website)
-time.sleep(3)
-driver.find_element(by=By.XPATH, value="//*[@id=\"__next\"]/main/div/div/button").click()
-time.sleep(3)
-driver.find_element(by=By.XPATH, value="//*[@id=\"__next\"]/main/div/div/button").click()
-time.sleep(3)
-driver.find_element(by=By.XPATH, value="//*[@id=\"__next\"]/main/div/div/div[1]/div[1]/div[2]/button[2]").click()
-time.sleep(3)
-driver.find_element(by=By.XPATH, value="//*[@id=\"__next\"]/main/div/div/div/div/div/div[1]/div/div[1]/button[2]").click()
-time.sleep(3)
-driver.find_element(by=By.XPATH, value="//*[@id=\"email\"]").send_keys(env["Facebook_Em"])
-time.sleep(3)
-driver.find_element(by=By.XPATH, value="//*[@id=\"pass\"]").send_keys(env["Facebook_Pw"])
-time.sleep(3)
-driver.find_element(by=By.XPATH, value="//*[@id=\"loginbutton\"]").click()
-time.sleep(3)
-driver.find_element(by=By.XPATH, value="""//*[@id="mount_0_0_QY"]/div/div/div/div/div/div/div[1]/div/div/div/div/div/div/div/div/div/div[3]/div[1]/div/div/div/div[1]""").click()
-# //*[@id="loginbutton"]
+  return driver
 
-time.sleep(200)
+def speak(driver, msg):
+  driver.find_element(by=By.XPATH, value="//*[@id=\"__next\"]/main/div/div/div[3]/div[1]/div[4]/div/div/textarea").send_keys(f"Reply with what is in the quotes AND NOTHING ELSE. \"{msg}\"")
+  time.sleep(1)
+  driver.find_element(by=By.XPATH, value="//*[@id=\"__next\"]/main/div/div/div[3]/div[1]/div[4]/div/button").click()
+  driver.find_element(by=By.XPATH, value="//*[@id=\"__next\"]/main/div/div/div[3]/div[1]/div[4]/div/button").click()
+  driver.find_element(by=By.XPATH, value="//*[@id=\"__next\"]/main/div/div/div[3]/div[1]/div[4]/div/button").click()
+
+if __name__ == '__main__':
+  print('Initiating setup....')
+  driver = setup()
+  print('Setup Complete!')
+  
+  while True: speak(driver, input(">>> "))
